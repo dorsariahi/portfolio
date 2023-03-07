af6713fac72a647270203e33e1d0df5036936f11
dorsariahi <dorsariahiasl@gmail.com>
2023-03-07 12:19:14 -0600

    final push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import './App.css';
+import Main from './components/App';
+import './index.css'
+
+function App() {
+  return (
+   <Main />
+  );
+}
+
+export default App;
